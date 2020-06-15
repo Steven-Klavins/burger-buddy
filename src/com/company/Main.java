9487c7a661d5f346d566fe1656abcaa3c10c74c4
Steven <stevenklavins94@gmail.com>
2020-06-15 09:10:45 +0100

    feat: added print menu method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,23 @@
 package com.company;
 
+import java.lang.reflect.Array;
+import java.lang.reflect.Member;
+
 public class Main {
+    public static String [] MENU = new String[] {"Burgers", "Milkshakes", "Extras", "Desserts", "Meal Deals"};
 
     public static void main(String[] args) {
-
+        printMenu();
 
     }
+
+    public static void printMenu(){
+        System.out.println("*---------------------Menu---------------------*");
+        System.out.println("Enter the name of the item you would like to view");
+        for (int i = 0; i< MENU.length; i++) {
+            System.out.print(MENU[i]+"\n");
+        };
+
+    };
 
 }
