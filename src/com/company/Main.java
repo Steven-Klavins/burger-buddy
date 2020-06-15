8e11a7d5b5df31381814ad31a795d94fe730e40d
Steven <stevenklavins94@gmail.com>
2020-06-15 11:44:26 +0100

    feat: user can now exit menu

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,34 +5,36 @@ import java.lang.reflect.Member;
 import java.util.Scanner;
 
 public class Main {
-    public static String [] MENU = new String[] {"#1 Burgers", "#2 Milkshakes", "#3 Extras", "#4 Desserts", "#5 Meal Deals", "#6 Exit"};
+    public static String[] MENU = new String[]{"#1 Burgers", "#2 Milkshakes", "#3 Extras", "#4 Desserts", "#5 Meal Deals", "#6 Exit"};
 
     public static void main(String[] args) {
-        double total = 0.00;
-        Scanner scanner = new Scanner(System.in);
-        int choice = scanner.nextInt();
         user();
-
+        double total = 0.00;
     }
 
-    public static void printMenu(){
+    public static void printMenu() {
         System.out.println("*---------------------Menu---------------------*");
         System.out.println("Select an item");
-        for (int i = 0; i< MENU.length; i++) {
-            System.out.print(MENU[i]+"\n");
+        for (int i = 0; i < MENU.length; i++) {
+            System.out.print(MENU[i] + "\n");
         };
-
     };
 
-    public static void user () {
+    public static void user() {
         boolean finished = false;
         printMenu();
-        while (!finished){
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
+        while (finished==false) {
 
+            switch (choice) {
+                case 6:
+                    System.out.println("Goodbye!");
+                    break;
+                default:
+                    System.out.println("Please enter a valid selection");
+            }
+            finished = true;
         }
-
     }
-
-
-
 }
