9e4088e391a502d5f63d861a9bf11fe8844b9ffb
Steven <stevenklavins94@gmail.com>
2020-06-18 16:02:23 +0100

    feat: total for order shows

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,15 +1,18 @@
 package com.company;
 
-import java.lang.reflect.Array;
-import java.lang.reflect.Member;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static String[] MENU = new String[]{"#1 Burgers", "#2 Milkshakes", "#3 Extras", "#4 Desserts", "#5 Meal Deals", "#6 Exit"};
-    public static String[] BURGERS = new String[]{"#1 Cheese burger - £7.00", "#2 Beef burger","#3 Chicken burger - £7.00", "#4 Veggie burger"};
+    public static String[] BURGERS = new String[]{"#1 Cheese burger - £7.00", "#2 Beef burger - £5.00","#3 Chicken burger - £7.00", "#4 Veggie burger - £5.00"};
+    public static double total = 0.00;
+    public static List<String> items = new ArrayList<>();
+
     public static void main(String[] args) {
         user();
-        double total = 0.00;
+
     }
 
     public static void printMenu() {
@@ -24,20 +27,21 @@ public class Main {
         boolean finished = false;
         printMenu();
         Scanner scanner = new Scanner(System.in);
-        int choice = scanner.nextInt();
-        while (finished==false) {
 
+        while (finished==false) {
+            int choice = scanner.nextInt();
             switch (choice) {
                 case 1:
                    burgers();
                     break;
                 case 6:
                     System.out.println("Goodbye!");
+                    finished = true;
                     break;
                 default:
                     System.out.println("Please enter a valid selection");
             }
-            finished = true;
+
         }
     }
 
@@ -47,6 +51,27 @@ public class Main {
         for (int i = 0; i < BURGERS.length; i++) {
             System.out.print(BURGERS[i] + "\n");
         };
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt()-1;
 
+        if(choice == 0 || choice == 2){
+            total += 7.00;
+            items.add(BURGERS[choice]);
+        }
+        else {
+            total += 5.00;
+            items.add(BURGERS[choice]);
+        }
+        printOrder ();
+        printMenu();
+
+    }
+
+    public static void printOrder () {
+        System.out.println("*---------------------order---------------------*");
+        for (int i = 0; i < items.size(); i++) {
+            System.out.print(items.get(i) + "\n");
+        };
+        System.out.println("Total: " + total);
     }
 }
